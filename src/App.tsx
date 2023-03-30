a5d937131f29495658b54a3247aad53b4351d1cc
Muhammad Bilawal <bilawal225@gmail.com>
2023-03-30 20:03:37 +0500

    lesson of styleprops in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 
 import './App.css';
 import { Button } from './components/Button';
+import Container from './components/Container';
 import Input from './components/Input';
 function App() {
 
   return (
     <div className="App">
-      <Button 
-      handleClick={(event,id)=>{
-        console.log('Hello Bilawal',event,id)
-      }}/>
-      <Input value='' handleChange={(event)=>console.log(event)}/>
-    </div>
+      <Container styles={{border:'1px solid black', padding:'1rem'}}/>
+     </div>
   );
 }
 
